f1008366ed1fd7a6f3357df454ccc7131ac27939
Aron Ilie <aronilie.code@gmail.com>
2022-08-30 19:38:23 +0200

    Created general styles

---
+++ src/styles/styles.ts
@@ -0,0 +1,27 @@
+const styles = {
+  colors: {
+    main: "#5A60FF",
+    mainAction: "#787DFF",
+    grey: "#F1F1F1",
+    red: "#FF2849",
+    redAction: "#FF4A66",
+    black: "#151515",
+    placeholder: "#888888",
+  },
+  fontSizes: {
+    extraSmaller: "12px",
+    smaller: "14px",
+    small: "16px",
+    medium: "18px",
+    big: "20px",
+    bigger: "24px",
+  },
+  fontWeights: {
+    extraBold: "800",
+    bold: "700",
+    semiBold: "600",
+    regular: "400",
+  },
+};
+
+export default styles;
