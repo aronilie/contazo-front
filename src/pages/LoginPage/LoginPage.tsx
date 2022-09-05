4bfabf0a3114af7a0e0edf028a53d5682e40fd80
Aron Ilie <aronilie.code@gmail.com>
2022-09-05 16:01:46 +0200

    Added Header component to LoginPage and RegisterPage components

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,11 @@
+import Header from "../../components/Header/Header";
 import Login from "../../components/Login/Login";
 import Navigation from "../../components/Navigation/Navigation";
 
 const LoginPage = (): JSX.Element => {
   return (
     <>
+      <Header />
       <Login />
       <Navigation />
     </>
