51e1d005ae374bf251cc6b1ab5c1dfc947520449
Aron Ilie <aronilie.code@gmail.com>
2022-09-05 10:32:54 +0200

    Created LoginPage page

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,13 @@
+import Login from "../../components/Login/Login";
+import Navigation from "../../components/Navigation/Navigation";
+
+const LoginPage = (): JSX.Element => {
+  return (
+    <>
+      <Login />
+      <Navigation />
+    </>
+  );
+};
+
+export default LoginPage;
