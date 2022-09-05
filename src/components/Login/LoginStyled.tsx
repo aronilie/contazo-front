1bbb3be42e15791aa8815d3b70d900fc62d64775
Aron Ilie <aronilie.code@gmail.com>
2022-09-05 13:17:20 +0200

    Deleted button styles of Login and Register components

--- src/components/Login/LoginStyled.tsx
+++ src/components/Login/LoginStyled.tsx
@@ -84,24 +84,6 @@ const LoginStyled = styled.div`
       }
     }
 
-    &__button {
-      min-height: 3.25em;
-      width: 100%;
-      margin: 1em 0;
-      border-radius: 0.4em;
-      border: none;
-      cursor: pointer;
-      background-color: ${styles.colors.main};
-      color: ${styles.colors.white};
-      font-family: "Inter", sans-serif;
-      font-size: ${styles.fontSizes.small};
-      font-weight: ${styles.fontWeights.semiBold};
-
-      @media (min-width: 800px) {
-        max-width: 700px;
-      }
-    }
-
     &-button__error--active {
       background-color: ${styles.colors.mainAction};
     }
