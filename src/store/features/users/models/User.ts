0a9b44aadf8c8a2ae19ed91ee6dafaf3cdb6e351
Aron Ilie <aronilie.code@gmail.com>
2022-09-05 10:06:50 +0200

    Created Wrapper component

---
+++ src/store/features/users/models/User.ts
@@ -0,0 +1,22 @@
+export interface UserData {
+  name?: string;
+  surname?: string;
+  email?: string;
+  phoneNumber: string;
+  password: string;
+}
+
+export interface RegisterUserData {
+  phoneNumber: string;
+  id: string;
+}
+
+export interface LoginUserData {
+  phoneNumber: string;
+  token: string;
+  id: string;
+}
+
+export interface UserToken {
+  token: string;
+}
