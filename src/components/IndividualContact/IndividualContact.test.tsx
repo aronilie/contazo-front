2796e75aa0034c4b8321c4842dbb2b914a845aa1
Aron Ilie <aronilie.code@gmail.com>
2022-09-10 22:26:30 +0200

    Tested navigation of IndividualContact component

--- src/components/IndividualContact/IndividualContact.test.tsx
+++ src/components/IndividualContact/IndividualContact.test.tsx
@@ -1,5 +1,8 @@
 import IndividualContact from "./IndividualContact";
 import { render, screen } from "@testing-library/react";
+import * as router from "react-router";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
 
 const contact = {
   name: "Dan",
@@ -8,16 +11,41 @@ const contact = {
   phoneNumber: "888555222",
   owner: "owner",
 };
+
+const navigate = jest.fn();
+beforeEach(() => {
+  jest.spyOn(router, "useNavigate").mockImplementation(() => navigate);
+});
+
 describe("Given a IndividualContact component", () => {
   describe("When it is instantiated with a contact", () => {
-    test.only("Then it should render the 'email' and the 'phoneNumber' of the contact", async () => {
-      render(<IndividualContact contact={contact} />);
+    test("Then it should render the 'email' and the 'phoneNumber' of the contact", async () => {
+      render(
+        <MemoryRouter>
+          <IndividualContact contact={contact} />
+        </MemoryRouter>
+      );
 
       const phoneNumber = screen.getByText("888555222");
       const email = screen.getByText("dan@test.com");
 
       expect(phoneNumber).toBeInTheDocument();
       expect(email).toBeInTheDocument();
+    });
+
+    describe("And the user clicks in one contact", () => {
+      test("Then it should navigate to the contact details page", async () => {
+        render(
+          <MemoryRouter>
+            <IndividualContact contact={contact} />
+          </MemoryRouter>
+        );
+
+        const contactReceived = screen.getByRole("listitem");
+        await userEvent.click(contactReceived);
+
+        expect(navigate).toHaveBeenCalledTimes(1);
+      });
     });
   });
 });
