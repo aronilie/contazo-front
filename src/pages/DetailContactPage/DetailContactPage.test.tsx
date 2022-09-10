1317efac19f9a8089a6aef3c7dddb6d8c82914f5
Aron Ilie <aronilie.code@gmail.com>
2022-09-10 22:29:00 +0200

    Tested DetailContactPage component

--- src/pages/DetailContactPage/DetailContactPage.test.tsx
+++ src/pages/DetailContactPage/DetailContactPage.test.tsx
@@ -4,15 +4,31 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "../../app/store";
 import DetailContactPage from "./DetailContactPage";
 
+const contact = {
+  name: "Dan",
+  surname: "Abramov",
+  email: "dan@test.com",
+  phoneNumber: "888555222",
+  owner: "owner",
+};
+
+let mockGetByPhoneNumberFunction = {
+  getContactByPhoneNumber: jest.fn().mockReturnValue(contact),
+};
+jest.mock(
+  "../../features/contacts/hooks/useContactsApi",
+  () => () => mockGetByPhoneNumberFunction
+);
+
 describe("Given a DetailContactPage component", () => {
   describe("When it is instantiated", () => {
-    test("Then it should show the text of navigation 'Contacts'", () => {
+    test("Then it should render the information of the contact passed for props", () => {
       render(
-        <Provider store={store}>
-          <BrowserRouter>
+        <BrowserRouter>
+          <Provider store={store}>
             <DetailContactPage />
-          </BrowserRouter>
-        </Provider>
+          </Provider>
+        </BrowserRouter>
       );
 
       const expectedText = screen.getByText("Contacts");
