38e6a364f8d26e6c46695832312c54656288dc23
Aron Ilie <aronilie.code@gmail.com>
2022-09-12 13:58:29 +0200

    Added logout link to Navigation component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,29 @@
-import { useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import useUserApi from "../../features/users/hooks/useUserApi";
 import HeaderStyled from "./HeaderStyled";
 
 const Header = (): JSX.Element => {
   const { pathname } = useLocation();
+  const { logout } = useUserApi();
+
+  const handleLogout = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    logout();
+  };
 
   const isRendered = pathname !== "/register" && pathname !== "/login";
 
   return (
     <>
-      {isRendered && (
-        <HeaderStyled className="header">
-          <img
-            src="/img/logo.png"
-            alt="Contazo logo"
-            className="header__logo"
-          />
-        </HeaderStyled>
-      )}
+      <HeaderStyled className="header">
+        <img src="/img/logo.png" alt="Contazo logo" className="header__logo" />
+        {isRendered && (
+          <NavLink to="/login" className="link" onClick={handleLogout}>
+            <span className="link__text">Logout</span>
+          </NavLink>
+        )}
+      </HeaderStyled>
     </>
   );
 };
